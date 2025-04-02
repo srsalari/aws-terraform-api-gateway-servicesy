fee1f7b5b3ed24862f0536db28d192f9663694aa
srsalari <sr_salari@hotmail.com>
2025-04-01 21:34:49 -0400

    Modules, variables and output files implemented

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
-variable "region" {
-  description = "The AWS region to deploy resources"
-  default     = "ca-central-1"
-}
-
 provider "aws" {
-  region = "ca-central-1"
+  region = var.region
 }
 
 # IAM Role for Lambda
@@ -38,14 +33,12 @@ resource "aws_iam_policy_attachment" "lambda_basic_policy" {
 
 // Bucket used for Glue scripts and general services
 resource "aws_s3_bucket" "data_bucket" {
-  bucket = "saeed-terraform-app-data-storage"
-  acl    = "private"
+  bucket = var.data_bucket_name
 }
 
 // Bucket used exclusively for file uploads via the process_s3 Lambda
 resource "aws_s3_bucket" "upload_bucket" {
-  bucket = "saeed-app-data-storage-2025"
-  acl    = "private"
+  bucket = var.upload_bucket_name
 }
 
 # DynamoDB Table
@@ -60,38 +53,9 @@ resource "aws_dynamodb_table" "records_table" {
   }
 }
 
-# Lambda Functions
-resource "aws_lambda_function" "process_dynamodb" {
-  function_name = "ProcessDynamoDB"
-  runtime       = "python3.8"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "process_dynamodb.lambda_handler"
-  filename      = "process_dynamodb.zip"
-
-  source_code_hash = filebase64sha256("process_dynamodb.zip")
-}
-
-resource "aws_lambda_function" "process_s3" {
-  function_name    = "ProcessS3"
-  runtime          = "python3.8"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "process_s3.lambda_handler"
-  filename         = "process_s3.zip"
-  source_code_hash = filebase64sha256("process_s3.zip")
-  publish          = true
-}
-
-resource "aws_lambda_function" "start_glue" {
-  function_name = "StartGlueJob"
-  runtime       = "python3.8"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "start_glue.lambda_handler"
-  filename      = "start_glue.zip"
-}
-
 # API Gateway
 resource "aws_api_gateway_rest_api" "api" {
-  name        = "MyAPI"
+  name        = var.api_name
   description = "API Gateway for my AWS workflow"
 }
 
@@ -114,7 +78,7 @@ resource "aws_api_gateway_integration" "lambda_records" {
   http_method             = aws_api_gateway_method.get_records.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.process_dynamodb.invoke_arn
+  uri                     = module.functions.process_dynamodb_invoke_arn
 }
 
 resource "aws_api_gateway_resource" "glue" {
@@ -136,7 +100,7 @@ resource "aws_api_gateway_integration" "lambda_glue" {
   http_method             = aws_api_gateway_method.post_glue.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.start_glue.invoke_arn
+  uri                     = module.functions.start_glue_invoke_arn
 }
 
 // New S3 Resource
@@ -154,25 +118,20 @@ resource "aws_api_gateway_method" "get_s3" {
   authorization = "NONE"
 }
 
-// AWS Service Integration with S3 to perform ListBucket operation
 resource "aws_api_gateway_integration" "s3_get" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.s3.id
   http_method             = aws_api_gateway_method.get_s3.http_method
   type                    = "AWS"
   integration_http_method = "GET"
-  uri                     = "arn:aws:apigateway:${var.region != "" ? var.region : "ca-central-1"}:s3:path/${aws_s3_bucket.data_bucket.bucket}"
-
-  // Pass query parameter to list objects (ListBucket - ListType V2)
+  uri                     = "arn:aws:apigateway:${var.region}:s3:path/${var.data_bucket_name}"
   request_parameters = {
     "integration.request.querystring.list-type" = "'2'"
   }
-
-  // Use the IAM role to sign the request
   credentials = aws_iam_role.apigw_s3_role.arn
 }
 
-// Method Response for 200 OK
+# Method Response for 200 OK on GET /s3
 resource "aws_api_gateway_method_response" "get_s3_response" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.s3.id
@@ -183,7 +142,7 @@ resource "aws_api_gateway_method_response" "get_s3_response" {
   }
 }
 
-// Integration Response for 200 OK
+# Integration Response for 200 OK on GET /s3
 resource "aws_api_gateway_integration_response" "get_s3_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.s3.id
@@ -192,11 +151,10 @@ resource "aws_api_gateway_integration_response" "get_s3_integration_response" {
   response_templates = {
     "application/json" = ""
   }
-
   depends_on = [aws_api_gateway_integration.s3_get]
 }
 
-// POST method on /s3 to obtain a pre-signed URL for file upload
+# POST method on /s3 to obtain a pre-signed URL for file upload
 resource "aws_api_gateway_method" "post_s3" {
   rest_api_id   = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.s3.id
@@ -204,175 +162,13 @@ resource "aws_api_gateway_method" "post_s3" {
   authorization = "NONE"
 }
 
-// Integration for the POST method to invoke the process_s3 Lambda
 resource "aws_api_gateway_integration" "lambda_s3" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.s3.id
   http_method             = aws_api_gateway_method.post_s3.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.process_s3.invoke_arn
-}
-
-// Lambda permission to allow API Gateway to invoke process_s3
-resource "aws_lambda_permission" "apigw_lambda_s3" {
-  statement_id  = "AllowAPIGatewayInvokeS3"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.process_s3.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
-}
-
-# Deploy API (Ensure API Gateway has methods before deploying)
-resource "aws_api_gateway_deployment" "deployment" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  stage_name  = "prod"
-
-  triggers = {
-    redeployment = sha1(jsonencode({
-      glue_integration_uri    = aws_api_gateway_integration.lambda_glue.uri,
-      records_integration_uri = aws_api_gateway_integration.lambda_records.uri,
-      s3_integration_uri      = aws_api_gateway_integration.lambda_s3.uri
-    }))
-  }
-
-  depends_on = [
-    aws_api_gateway_integration.lambda_records,
-    aws_api_gateway_integration.lambda_glue,
-    aws_api_gateway_integration.lambda_s3
-  ]
-}
-
-# AWS Glue Job
-resource "aws_glue_job" "glue_job" {
-  name     = "HelloWorldJob"
-  role_arn = aws_iam_role.lambda_role.arn
-  command {
-    script_location = "s3://${aws_s3_bucket.data_bucket.bucket}/scripts/glue_script.py"
-    name            = "glueetl"
-  }
-}
-
-resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.process_dynamodb.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
-}
-
-resource "aws_lambda_permission" "apigw_lambda_glue" {
-  statement_id  = "AllowAPIGatewayInvokeGlue"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.start_glue.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
-}
-
-resource "aws_iam_policy" "dynamodb_policy" {
-  name        = "lambda_dynamodb_policy"
-  description = "Policy for Lambda to perform PutItem on RecordsTable"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "dynamodb:PutItem",
-          "dynamodb:GetItem",
-          "dynamodb:UpdateItem",
-          "dynamodb:DeleteItem",
-          "dynamodb:Scan",
-          "dynamodb:Query"
-        ],
-        Resource = aws_dynamodb_table.records_table.arn
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "lambda_dynamodb_policy_attach" {
-  name       = "lambda_dynamodb_policy_attachment"
-  roles      = [aws_iam_role.lambda_role.name]
-  policy_arn = aws_iam_policy.dynamodb_policy.arn
-}
-
-resource "aws_iam_policy" "glue_policy" {
-  name        = "lambda_glue_policy"
-  description = "Policy for Lambda to execute a Glue job"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Action = [
-          "glue:StartJobRun",
-          "glue:GetJobRun",
-          "glue:GetJobRuns"
-        ],
-        Resource = aws_glue_job.glue_job.arn
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "glue_policy_attachment" {
-  name       = "lambda_glue_policy_attach"
-  roles      = [aws_iam_role.lambda_role.name]
-  policy_arn = aws_iam_policy.glue_policy.arn
-}
-
-// Policy to allow updating Lambda function code and configuration
-resource "aws_iam_policy" "lambda_update_policy" {
-  name        = "lambda_update_policy"
-  description = "Policy for updating Lambda function code and configuration"
-  policy = jsonencode({
-    Version : "2012-10-17",
-    Statement : [
-      {
-        Effect : "Allow",
-        Action : [
-          "lambda:UpdateFunctionCode",
-          "lambda:UpdateFunctionConfiguration",
-          "lambda:PublishVersion"
-        ],
-        Resource : "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "lambda_update_policy_attach" {
-  name       = "lambda_update_policy_attachment"
-  roles      = [aws_iam_role.lambda_role.name]
-  policy_arn = aws_iam_policy.lambda_update_policy.arn
-}
-
-// (Optional) Policy to allow updating S3 bucket policy, if needed
-resource "aws_iam_policy" "s3_policy_update" {
-  name        = "s3_policy_update"
-  description = "Policy to allow updating the bucket policy for data_bucket"
-  policy = jsonencode({
-    Version : "2012-10-17",
-    Statement : [
-      {
-        Effect : "Allow",
-        Action : [
-          "s3:PutBucketPolicy",
-          "s3:GetBucketPolicy",
-          "s3:ListBucket"
-        ],
-        Resource : aws_s3_bucket.data_bucket.arn
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy_attachment" "s3_policy_update_attach" {
-  name       = "s3_policy_update_attachment"
-  roles      = [aws_iam_role.lambda_role.name] // Adjust if you want to allow a different role/admin user to update S3 policies
-  policy_arn = aws_iam_policy.s3_policy_update.arn
+  uri                     = module.functions.process_s3_invoke_arn
 }
 
 // IAM Role for API Gateway to access S3
@@ -380,79 +176,86 @@ resource "aws_iam_role" "apigw_s3_role" {
   name = "apigw_s3_role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "apigateway.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
+    Statement = [{
+      Effect    = "Allow",
+      Principal = { Service = "apigateway.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
   })
 }
 
-// Attach an inline policy to allow listing the bucket
+# Inline policy for the API Gateway S3 role
 resource "aws_iam_role_policy" "apigw_s3_role_policy" {
   name = "apigw_s3_role_policy"
   role = aws_iam_role.apigw_s3_role.id
   policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = ["s3:ListBucket"],
-        Resource = [aws_s3_bucket.data_bucket.arn]
-      }
-    ]
+    Statement = [{
+      Effect   = "Allow",
+      Action   = ["s3:ListBucket"],
+      Resource = [aws_s3_bucket.data_bucket.arn]
+    }]
   })
 }
 
-// S3 Bucket Policy to allow the API Gateway IAM role to list objects
 resource "aws_s3_bucket_policy" "data_bucket_policy" {
   bucket = aws_s3_bucket.data_bucket.id
   policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Sid    = "AllowAPIGatewayList",
-        Effect = "Allow",
-        Principal = {
-          AWS = aws_iam_role.apigw_s3_role.arn
-        },
-        Action   = "s3:ListBucket",
-        Resource = aws_s3_bucket.data_bucket.arn
-      }
-    ]
+    Statement = [{
+      Sid       = "AllowAPIGatewayList",
+      Effect    = "Allow",
+      Principal = { AWS = aws_iam_role.apigw_s3_role.arn },
+      Action    = "s3:ListBucket",
+      Resource  = aws_s3_bucket.data_bucket.arn
+    }]
   })
 }
 
-output "api_url" {
-  description = "Base URL for the API Gateway deployment"
-  value       = aws_api_gateway_deployment.deployment.invoke_url
+# Deploy API (Ensure API Gateway has methods before deploying)
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  triggers = {
+    redeployment = sha1(jsonencode({
+      records_integration = aws_api_gateway_integration.lambda_records.uri,
+      glue_integration    = aws_api_gateway_integration.lambda_glue.uri,
+      s3_integration      = aws_api_gateway_integration.lambda_s3.uri
+    }))
+  }
+  depends_on = [
+    aws_api_gateway_integration.lambda_records,
+    aws_api_gateway_integration.lambda_glue,
+    aws_api_gateway_integration.lambda_s3
+  ]
 }
 
-output "glue_job_name" {
-  description = "Name of the Glue job"
-  value       = aws_glue_job.glue_job.name
+resource "aws_api_gateway_stage" "stage" {
+  deployment_id = aws_api_gateway_deployment.deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = var.api_stage
 }
 
-output "s3_api_url" {
-  description = "API endpoint URL for S3 service (listing and uploads)"
-  value       = "${aws_api_gateway_deployment.deployment.invoke_url}/s3"
+# AWS Glue Job
+resource "aws_glue_job" "glue_job" {
+  name     = var.glue_job_name
+  role_arn = aws_iam_role.lambda_role.arn
+  command {
+    script_location = "s3://${var.data_bucket_name}/${var.glue_script_location}"
+    name            = "glueetl"
+  }
 }
 
-output "data_bucket_name" {
-  description = "Name of the S3 bucket for Glue scripts and general services"
-  value       = aws_s3_bucket.data_bucket.bucket
-}
+# Lambda permission for process_dynamodb (managed in module)
+# Lambda permission for start_glue (managed in module)
+# Lambda permission for process_s3 (managed in module)
 
-output "upload_bucket_name" {
-  description = "Name of the S3 bucket used exclusively for file uploads via ProcessS3"
-  value       = aws_s3_bucket.upload_bucket.bucket
-}
-
-output "upload_api_url" {
-  description = "API endpoint URL for S3 upload service (POST method to obtain a pre-signed URL)"
-  value       = "${aws_api_gateway_deployment.deployment.invoke_url}/s3"
+# Module call for Lambda functions and permissions
+module "functions" {
+  source                  = "./modules/functions"
+  lambda_runtime          = var.lambda_runtime
+  process_dynamodb_zip    = var.process_dynamodb_zip
+  process_s3_zip          = var.process_s3_zip
+  start_glue_zip          = var.start_glue_zip
+  lambda_role_arn         = aws_iam_role.lambda_role.arn
+  api_gateway_execute_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 }
